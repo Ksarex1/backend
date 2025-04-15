0688287d60e11281ca713efaa824bf862b2a1bee
Roman Tkachev <romatromikfak@gmail.com>
2025-04-15 14:47:47 +0300

    Fixed 4

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://front-plum-six.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Добавляем PATCH
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
