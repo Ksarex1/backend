37eda3d0abccc3daa61573eaa11734a36a2218b5
Roman Tkachev <romatromikfak@gmail.com>
2025-04-15 19:01:16 +0300

    123

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -6,10 +6,12 @@ export default (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, "secret123")
+            console.log("Token decoded successfully:", decoded);
             req.userId = decoded._id
             next()
 
         } catch (error) {
+            console.error("Token verification failed:", error.message);
             return res.status(403).json({
                 message: "Нет доступа"
             })
